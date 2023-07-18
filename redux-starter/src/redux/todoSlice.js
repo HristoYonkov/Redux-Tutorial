37ad388398005bfb43cc7fad4035bf4556b18033
Hristo Yonkov <hristopturs@gmail.com>
2023-07-18 13:42:01 +0300

    Update todoSlice.js

--- redux-starter/src/redux/todoSlice.js
+++ redux-starter/src/redux/todoSlice.js
@@ -98,6 +98,7 @@ const todoSlice = createSlice({
     extraReducers(builder) {
         builder.addCase(getTodosAsync.pending, (state, action) => {
             console.log('Fetching data...');
+            // return loader boolean
         });
         builder.addCase(getTodosAsync.fulfilled, (state, action) => {
             console.log('Fetched data successfully!');
